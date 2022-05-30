6b2fc99dc78bc59b18e8e097d9f81f146f08dcf8
Rhandle <91404648+Rhandle@users.noreply.github.com>
2022-05-30 21:24:59 +0400

    Delete Gapuz Web Portfolio directory

--- Gapuz Web Portfolio/galleryfull.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('resize', resizeAll);
-gallery.querySelectorAll('.gallery-item').forEach(function (item) {
-    item.addEventListener('click', function () {        
-        item.classList.toggle('full');
-        item.style.zIndex = "1000";      
-    });
-});
