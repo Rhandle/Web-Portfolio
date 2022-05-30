b048cca8b1c7f9d7556db408efa263f5e080af98
Rhandle <91404648+Rhandle@users.noreply.github.com>
2022-05-30 20:32:20 +0400

    Add files via upload

---
+++ Gapuz Web Portfolio/galleryfull.js
@@ -0,0 +1,7 @@
+window.addEventListener('resize', resizeAll);
+gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+    item.addEventListener('click', function () {        
+        item.classList.toggle('full');
+        item.style.zIndex = "1000";      
+    });
+});
